0c4a230fee2c8d5a11593bd887c51f4be46b6316
Dylan <48769007+boy79582@users.noreply.github.com>
2022-12-15 22:17:37 +0800

    Add select data function, DB_personnelData.cs  for DBconnections, datatables.js, moment.js;

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YC_Test.Models;
 
 namespace YC_Test.Controllers
 {
     public class HomeController : Controller
     {
+        DB_personnelData DB_personnelData = new Models.DB_personnelData();
+
         public ActionResult Index()
         {
             return View();
         }
-     
+
+        public string getData()
+        {
+            var DT = DB_personnelData.getData();
+            return JsonConvert.SerializeObject(DT);
+        }     
     }
 }
---
+++ Models/DB_personnelData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Web;
+using Dapper;
+using Newtonsoft.Json;
+
+namespace YC_Test.Models
+{
+    public class DB_personnelData
+    {
+        string ConnString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public DataTable getData()
+        {
+            var Result = new DataTable();
+            using (SqlConnection conn = new SqlConnection(ConnString))
+            {
+                string strSql = "Select * from [dbo].[Table]";
+                var results = conn.Query(strSql);
+                var json = JsonConvert.SerializeObject(results);
+                var dt = JsonConvert.DeserializeObject<DataTable>(json);
+                return dt;
+            }
+        }
+    }
+}
